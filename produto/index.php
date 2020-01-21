df2686c2108dd48c65cc153e5db0befe344763da
Luciane Souza <luciane.sz@hotmail.com>
2020-01-21 14:29:59 -0300

    imagens salvas

---
+++ produto/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+    <div class="container">
+      <ul class="nav nav bg-light justify-content-between">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Legumes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Verduras</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Frutas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ofertas</a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Ordenar por:</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Menor preço</a>
+          <a class="dropdown-item" href="#">Melhor avaliado</a>
+          <a class="dropdown-item" href="#">De A à Z</a>
+        </div>
+      </ul>
+
+      <div class="row mb-5">
+          <div class="col-sm-3">
+            <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">Batata Doce</h4>
+                  <h6 class="card-subtitle mb-2 text-muted">Loja do Zé</h6>
+                  <img class="card-img-top" src="./img/batatadoce.jpg" alt="">
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-3">
+            <div class="card">
+              <div class="card-body">
+                hjhjhjhjhjhjhjhjhh
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-3">
+            <div class="card">
+              <div class="card-body">
+                hjhjhjhjhjhjhjhjhh
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-3">
+            <div class="card">
+              <div class="card-body">
+                hjhjhjhjhjhjhjhjhh
+              </div>
+            </div>
+          </div>
+
+          
+
+          </div>
+
+      </div>
+
+      
+    </div>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
